311361ede52c0ddaa1598472c191bcb32fae51ae
FrosteNx <domr1a@wp.pl>
2024-03-24 01:13:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace Frostbot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
