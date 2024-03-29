b1a0fb3dae0116b9180a64dc99afcb2f19fd499c
FrosteNx <domr1a@wp.pl>
2024-03-29 22:50:51 +0100

    added basic mathematical commands

--- commands/TestCommands.cs
+++ commands/TestCommands.cs
@@ -1,15 +1,51 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using System;
 using System.Threading.Tasks;
 
 namespace Frostbot.commands
 {
     internal class TestCommands : BaseCommandModule
     {
-        [Command("test")] 
+        [Command("hello")] 
         public async Task MyFirstCommand(CommandContext ctx)
         {
             await ctx.Channel.SendMessageAsync($"Hello {ctx.User.Username}");
+        }
+
+        [Command("add")]
+        public async Task Add(CommandContext ctx, double x, double y)
+        {
+            double res = x + y;
+            await ctx.Channel.SendMessageAsync(res.ToString());
+        }
+
+        [Command("subtract")]
+        public async Task Subtract(CommandContext ctx, double x, double y)
+        {
+            double res = x - y;
+            await ctx.Channel.SendMessageAsync(res.ToString());
+        }
+
+        [Command("multiply")]
+        public async Task Multiply(CommandContext ctx, double x, double y)
+        {
+            double res = x * y;
+            await ctx.Channel.SendMessageAsync(res.ToString());
+        }
+
+        [Command("divide")]
+        public async Task Divide(CommandContext ctx, double x, double y)
+        {
+            double res = x / y;
+            await ctx.Channel.SendMessageAsync(res.ToString());
+        }
+
+        [Command("sqrt")]
+        public async Task SquareRoot(CommandContext ctx, double x)
+        {
+            double res = Math.Sqrt(x);
+            await ctx.Channel.SendMessageAsync(res.ToString());
         }
     }
 }
